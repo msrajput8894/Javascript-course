f2514c6eaffc6edb82f4ebd7624a3572494d30a2
mahendra rajput <msrajput8894@gmail.com>
2024-09-29 01:11:22 +0530

    Implemented revealing element on scroll feature using IntersectionObserver API

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -187,13 +187,35 @@ const obsNav = function (entries) {
   }
 };
 
-const observer = new IntersectionObserver(obsNav, {
+const headerObserver = new IntersectionObserver(obsNav, {
   root: null,
   threshold: 0,
   rootMargin: `-${navHeight}px`,
 });
 
-observer.observe(header);
+headerObserver.observe(header);
+
+// Reveal sections
+
+const allSections = document.querySelectorAll('.section');
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
 
 /////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////
