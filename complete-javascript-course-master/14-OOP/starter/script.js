d306054e05a2ed8bd093d7d1967bfe018136d91b
Mahendra Rajput <akashrajput1434@gmail.com>
2024-10-05 21:15:14 +0530

    OOP: completed coding challenge #2

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -402,3 +402,65 @@ gunjan.init('Gunjan', 2019);
 gunjan.calcAge();
 
 console.log(naitik);
+
+/////////////////////////////////////
+
+// Coding challenge #2
+
+/* 
+
+  1. Re-create challenge 1, but this time using an ES class;
+
+  2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
+
+  3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
+
+  4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
+
+  DATA CAR 1: 'FORD' going at 120 km/h
+*/
+
+class Car {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+const ford = new Car('Ford', 120);
+console.log(ford);
+
+ford.accelerate();
+ford.accelerate();
+
+ford.brake();
+ford.brake();
+ford.brake();
+
+console.log(ford.speedUS);
+
+ford.speedUS = 98.125; // this will set the speed of the ford to 98.125* 1.6 = 157;
+
+console.log(ford);
+
+ford.accelerate();
+console.log(ford);
+console.log(ford.speedUS);
