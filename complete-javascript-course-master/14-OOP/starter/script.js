df6a66ced91fdacfb5c70452f524b0f87d185b60
Mahendra Rajput <akashrajput1434@gmail.com>
2024-10-06 01:16:14 +0530

    OOP: Coding challenge #3 completed.

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -525,3 +525,76 @@ console.dir(Student.prototype.constructor); // Person, where it should be Studen
 
 Student.prototype.constructor = Student;
 console.dir(Student.prototype.constructor); // Student. Now it is correct!
+
+///////////////////////////////////////////////
+
+// Coding Challenge:
+
+/*
+1. Use a constructor function to implement an Electric Car (called EV) as child class of Car. Besides make and current speed , the EV also has the current battery charge in % ('charge' property);
+
+2. Implement a 'chargeBattery' method which takes an argument 'chargeTo';
+
+3. Implement an 'accelerate' method that will increase the car's speed by 20 and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 km/h, with a charge of 22%';
+
+4. Create an electric car object and exeperiment with calling 'accelerate', 'brake' and 'chargeBattery' (charge to 90%). Notice what happens when you 'accelerate'! Hint: review the definition of polymorphism
+
+DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
+
+*/
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed}`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed}`);
+};
+
+const ford = new Car('Ford', 120);
+
+ford.accelerate();
+ford.brake();
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+// this will link the prototypes of parent(Car) and child(EV)
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  return (this.charge = chargeTo);
+};
+
+const tesla = new EV('Tesla', 140, 23);
+
+console.log(tesla);
+
+tesla.chargeBattery(45);
+console.log(tesla);
+
+tesla.accelerate();
+tesla.brake();
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `${this.make} is going at ${this.speed} km/h, with a charge of ${this.charge}%`
+  );
+};
+
+// Now as we have overwritten the accelerate method, now the child(EV) will use the first method in prototype chain, which is currently on child(EV).
+tesla.accelerate();
+tesla.accelerate();
+tesla.accelerate();
+tesla.accelerate();
