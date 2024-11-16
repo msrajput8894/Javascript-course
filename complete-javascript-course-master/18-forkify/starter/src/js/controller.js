7ffa59b821e5d9d4392ce4f7a363caa0cb5827c2
mahendra rajput <akashrajput1434@gmail.com>
2024-11-16 23:01:56 +0530

    Used Publisher subsriber pattern to handle the events in MVC architecture

--- complete-javascript-course-master/18-forkify/starter/src/js/controller.js
+++ complete-javascript-course-master/18-forkify/starter/src/js/controller.js
@@ -3,14 +3,6 @@ import recipeView from './views/recipeView.js';
 import 'core-js';
 import 'regenerator-runtime/runtime';
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
@@ -28,10 +20,12 @@ const controlRecipes = async function () {
     // Rendering the recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    alert(err);
+    console.error(err);
   }
 };
 
-['hashchange', 'load'].forEach(ev =>
-  window.addEventListener(ev, controlRecipes)
-);
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+
+init();
